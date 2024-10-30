c7c61626db7c4c428dc98904558c06faf0e78766
Daniel Moreira <Dan198moreira@gmail.com>
2024-10-30 10:50:08 -0300

    Add files via upload

---
+++ Currículo/curriculo.js
@@ -0,0 +1,12 @@
+const nota = mb;
+if (nota === mb) {
+    console.log ("é nóis prof.")
+}
+ else if (nota === b) {
+    console.log ("pode ser mb né?")
+ }
+  else if (nota === r) {
+    console.log ("prof. arredonda pra um b")
+  } else {
+    console.log ("tenha piedade")
+  }
